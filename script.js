a9257eb9b0b18f69ef2aff54caaa9b63d77362e2
Saikat Roy <njmsaikat@gmail.com>
2020-06-26 15:12:45 +0600

    almost done

---
+++ script.js
@@ -0,0 +1,43 @@
+/*sticky navigation*/
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  //   console.log(oTop);
+
+  //   console.log(window.innerHeight);
+  //   console.log($(".section-2").offset().top);
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
+
+/*counter anime*/
+
+let nCount = function (selector) {
+  $(selector).each(function () {
+    $(this).animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 4000,
+        easing: "swing",
+        step: function (value) {
+          $(this).text(Math.ceil(value));
+        },
+      }
+    );
+  });
+};
+
+let a = 0;
+$(window).scroll(function () {
+  let oTop = $(".numbers").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    nCount(".rect>h1");
+  }
+});
