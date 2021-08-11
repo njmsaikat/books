8ca840def6a0b75a55a000426e6f2f1a3bbe4073
Saikat Roy <njmsaikat@gmail.com>
2021-08-11 23:41:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,4 @@ let nCount = function (selector) {
   });
 };
 
-let a = 0;
-$(window).scroll(function () {
-  let oTop = $(".numbers").offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() >= oTop) {
-    a++;
-    nCount(".rect>h1");
-  }
-});
+
